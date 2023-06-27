5cb3f58e218b1bd03d13b6296fd0ca9ab6a73c05
uchiborieri <zuumo219@gmail.com>
2023-06-27 17:01:23 +0900

    削除するとメールが送られる

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -15,7 +15,7 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
     end
@@ -23,8 +23,13 @@ class AgendasController < ApplicationController
 
   def destroy
     agenda = Agenda.find(params[:id])
+    team_members = agenda.team_members
+
     if agenda.user_id == current_user.id || agenda.team.owner_id == current_user.id
       agenda.destroy
+      team_members.each do | member |
+      AssignMailer.del_agenda_mail(member.email, agenda.title).deliver_now
+      end
       redirect_to dashboard_url, notice: 'アジェンダを削除しました。'
     else
       redirect_to dashboard_url, notice: 'アジェンダの作成者かチームリーダーのみ削除可能です'
