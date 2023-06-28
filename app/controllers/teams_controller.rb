d831db5fd6dd1b8c78178dc476f8d9256c8f2fb9
uchiborieri <zuumo219@gmail.com>
2023-06-28 13:43:18 +0900

    robcop

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -16,9 +16,9 @@ class TeamsController < ApplicationController
   end
 
   def edit
-    unless current_user == @team.owner
-      redirect_to team_path, notice: 'チームリーダーのみ編集可能'
-    end
+    return if current_user == @team.owner
+
+    redirect_to team_path, notice: 'チームリーダーのみ編集可能'
   end
 
   def create
